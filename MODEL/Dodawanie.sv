3aa4bb44a34e3e6cbabe5a21ea522e8e64e92613
adziezyk <01169018@pw.edu.pl>
2022-12-01 02:54:48 +0100

    Dodawanie

---
+++ MODEL/Dodawanie.sv
@@ -0,0 +1,14 @@
+module Dodawanie (i_argA, i_argB, i_carry, o_result, o_carry);
+parameter WIDTH=32;
+//inout logic              i_clk;
+//input logic              i_rsn;
+input logic              i_carry;
+input logic [WIDTH-1:0]  i_argA, i_argB;
+output logic [WIDTH-1:0] o_result;
+output logic             o_carry;
+always_comb
+begin
+{o_carry, o_result} = i_argA + i_argB + i_carry;
+end
+endmodule
+
