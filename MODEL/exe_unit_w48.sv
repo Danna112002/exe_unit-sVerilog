f05a661c1aa55d38c93a69762f914f46d28ba4e7
adziezyk <01169018@pw.edu.pl>
2022-12-14 02:20:11 +0100

    Pierwszy kompilowalny kod ALU

--- MODEL/exe_unit_w48.sv
+++ MODEL/exe_unit_w48.sv
@@ -1,50 +1,41 @@
-`include "alu_defines.vh"
-module exe_unit_w48 (i_argA, i_argB, i_oper, o_result, o_error, o_status, o_carry);
+module exe_unit_w48 (i_argA, i_argB, i_oper, o_result, o_status);
     parameter WIDTH = 32;
-    input logic  [`OPER_BITS:0] i_oper;
-    input logic                 i_rsn;
-    input logic                 i_clk;
-    input logic  [WIDTH-1:0]    i_argB;
-    input logic  [WIDTH-1:0]    i_argA;
-    output logic [WIDTH-1:0]    o_result;
-    output logic                o_error;
-    output logic [3:0]          o_status;
-    output logic                o_error_konw;
-    output logic                o_error_ust;
-    output logic                o_carry;
+    input logic              i_clk;
+    input logic  [WIDTH-1:0] i_oper;
+    input logic  [WIDTH-1:0] i_argB;
+    input logic  [WIDTH-1:0] i_argA;
+    output logic [WIDTH-1:0] o_result;
+    output logic [4:0]       o_status;
+    output logic             o_error_konw;
+    output logic             o_error_ust;
 
      logic [WIDTH-1:0] wynik;
-     logic             error;
+     logic [WIDTH-1:0] w_dod;
+     logic [WIDTH-1:0] w_por;
+     logic [WIDTH-1:0] w_ust;
+     logic [WIDTH-1:0] w_konw;
 
-    // jawne przypisanie portów wejścia/wyjścia do małych modułów
     Dodawanie           #(.WIDTH(WIDTH))        
-    Dodawanie            (.i_argA(i_argA), .i_argB(i_argB), .o_result(w_dod), .o_carry(o_carry));    
+    Dodawanie            (.i_argA(i_argA), .i_argB(i_argB), .o_result(w_dod), .o_carry(o_status[0]));    
     Porownywanie        #(.BITS(WIDTH))        
     Porownywanie         (.i_argA(i_argA), .i_argB(i_argB), .o_result(w_por));
     ustawianie          #(.BITS(WIDTH))        
     ustawianie           (.i_argA(i_argA), .i_argB(i_argB), .o_result(w_ust), .o_error(o_error_ust));
     konwersja           #(.WIDTH(WIDTH))  
-    konwersja            (.i_argA(i_argA), .i_argB(i_argB), .o_result(w_konw), .o_error(o_error_konw));
-
-    //blok kombinacyjny, pomocniecze zmienne wynik i error są wykorzystywane do zapisywania danych do rejestru
+    konwersja            (.i_argA(i_argA), .o_result(w_konw), .o_error(o_error_konw));
     always_comb
-begin
-case(i_oper)
-`ALU_ADD  : wynik=o_result;
-`ALU_COMP : wynik=o_result; 
-`ALU_CONV : wynik=o_result; 
-`ALU_SET  : wynik=o_result; 
-default :
-wynik=0;
-endcase
-
-end
-    //blok synchroniczny 
-    always_ff @(posedge i_clk) 
     begin
-        o_result<=wynik;
+        case(i_oper)
+        2'b00 : wynik=w_dod;
+        2'b01 : wynik=w_por; 
+        2'b10 : wynik=w_ust; 
+        2'b11 : wynik=w_konw; 
+        default 
+        wynik=0;
+        endcase
         
-
     end
-
+    always_ff @(posedge i_clk) begin
+    
+    end
 endmodule
